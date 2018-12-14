d8cba8f44dc6ba1295dad2c978799ed69bd0ca8a
Mwangaza Nuriath <mnuriath28@gmail.com>
2018-12-14 10:48:56 +0200

    add a button tag

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-function myFunction() {
-    var firstString = document.getElementById("myNumber").Value;
+//****************************/
+var triangles = function(){
+    var base = parseFloat(document.getElementById("base").value);
+    var height = parseFloat(document.getElementById("height").value);
 
- alert(firstString);
-}
+    var output = document.getElementById("output");
+
+    var area = 0.5 = base = height;
+
+
+ if(isNaN(base)) isNaN(hieght)){
+     output.textcontent = "you did not enter two number.";
+ }
+ else{
+     var area = 0.5 * base * height;
+     output.textcontent = "Area = " + area;
+ }
+};
