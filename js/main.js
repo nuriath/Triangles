b847037d262c45ce6440fefbc48c8541d65d2217
Mwangaza Nuriath <mnuriath28@gmail.com>
2018-12-14 12:33:12 +0200

    edit a value of triangle scalene

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 function myFunction (){
-    var number1 =parseInt(document.getElementById("myNumber1").Value);
-    var number2 =parseInt(document.getElementById("myNumber2").value);
-    var number3 =parseInt(document.getElementById("myNumber3").value);
+    var number1 =parseInt(document.getElementById("number1").value);
+    var number2 =parseInt(document.getElementById("number2").value);
+    var number3 =parseInt(document.getElementById("number3").value);
 
-    if (isEquilateral(number1 === number2 &&
-                      number1 === number3 && 
-                      number2 === number3)){
+    if (number1 === number2 && number1 === number3 && number2 === number3){
 
         alert ("Triangle Equilateral");
     }
-      else if (isScalene (number1+number2 > myNumber3 && 
-                        myNumber1+myNumber3 > myNumber2 && 
-                        myNumber2+myNumber3 > myNumber1)){
+      else if(number1+number2 !== number3 ||number1+number3 !==number2 || number2+number3 !== number1){
           alert ("Triangle Scalene")
 
     }
-      else if (isIsosceles (myNumber1 === myNumber2 && 
-                          myNumber1 >= mynumber3 && 
-                          myNumber2 >= mynumber3)){
+      else if (number1 === number2 && number1 < number3 && number2 < number3){
           alert ("Triangle Isosceles")
 
     }
